0a83e44a4e04c6bdce84a91b2ec279a25c5a8e7c
Igor Artamonov <igor@artamonov.ru>
2019-05-05 23:18:53 -0400

    solution: initial version

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export {GetRateRequest, GetRateReply, GetRateReplyItem} from './generated/prices_pb';
+export {IPricesClient, PricesClient} from './generated/prices_grpc_pb';
+export { credentials } from 'grpc';
