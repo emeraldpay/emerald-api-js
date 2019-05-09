bfce1b7e10e103083a8902445e836c616810160e
Igor Artamonov <igor@artamonov.ru>
2019-05-08 21:25:11 -0400

    solution: endpoints for blockchain, v0.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export {GetRateRequest, GetRateReply, GetRateReplyItem} from './generated/prices_pb';
 export {IPricesClient, PricesClient} from './generated/prices_grpc_pb';
+
+export {CallBlockchainRequest, CallBlockchainItem, CallBlockchainReplyItem} from './generated/blockchain_pb';
+export {IBlockchainClient, BlockchainClient} from './generated/blockchain_grpc_pb'
+
 export { credentials } from 'grpc';
