cee7b253e5db34876e6c9a752c8f00797fa1d855
Igor Artamonov <igor@artamonov.ru>
2019-05-12 23:59:07 -0400

    solution: address balance rpc version: 0.5.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-export {Chain} from './generated/common_pb';
+export {
+    Chain,
+    AnyAddress,
+    MultiAddress,
+    SingleAddress
+} from './generated/common_pb';
 
 export {GetRateRequest, GetRateReply, GetRateReplyItem} from './generated/prices_pb';
 export {IPricesClient, PricesClient} from './generated/prices_grpc_pb';
@@ -10,6 +15,9 @@ export {
     ChainHead
 } from './generated/blockchain_pb';
 export {IBlockchainClient, BlockchainClient} from './generated/blockchain_grpc_pb';
+
+export {TrackAccountRequest, TrackTxRequest, AccountStatus, TxStatus} from './generated/track_pb';
+export {ITrackClient, TrackClient} from './generated/track_grpc_pb';
 
 export {CHAINS, ChainSpec, chainByCode} from './chain';
 
