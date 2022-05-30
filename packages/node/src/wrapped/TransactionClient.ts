e40b57cd25e1bc6149fd01ca4cdcd4c476d94bb0
kan <kan@emerald.cash>
2022-05-30 15:31:41 +0300

    problem: convert is not initialized

--- packages/node/src/wrapped/TransactionClient.ts
+++ packages/node/src/wrapped/TransactionClient.ts
@@ -1,12 +1,13 @@
 import * as transaction_grpc_pb from "../generated/transaction_grpc_pb";
 import {callStream, NativeChannel} from "../channel";
+import {classFactory} from "./Factory";
 import * as grpc from "@grpc/grpc-js";
 import {alwaysRetry, ConnectionListener, Publisher, publishListToPromise, readOnce, transaction} from "@emeraldpay/api";
 
 export class TransactionClient {
     readonly client: transaction_grpc_pb.TransactionClient;
     readonly channel: NativeChannel;
-    readonly convert: transaction.Convert;
+    readonly convert: transaction.Convert = new transaction.Convert(classFactory);
 
     constructor(address: string, credentials: grpc.ChannelCredentials) {
         this.client = new transaction_grpc_pb.TransactionClient(address, credentials);
