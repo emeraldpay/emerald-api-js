eadd08f80ab53cd744ec71667c6480edcd5c6d5d
Sergei Novikov <boomer.create@gmail.com>
2023-05-30 16:11:57 +0300

    solution: get token only in signle client and share to anothers

--- packages/node/src/wrapped/Auth.ts
+++ packages/node/src/wrapped/Auth.ts
@@ -9,6 +9,7 @@ const { version: clientVersion } = require('../../package.json');
 export class AuthClient {
   readonly client: ProtoAuthClient;
   readonly channel: NativeChannel;
+  readonly credentials: ChannelCredentials;
   readonly retries: number;
 
   constructor(address: string, credentials: ChannelCredentials, agents: string[], retries = 3) {
@@ -16,6 +17,7 @@ export class AuthClient {
 
     this.client = new ProtoAuthClient(address, credentials, { 'grpc.primary_user_agent': agent });
     this.channel = new NativeChannel(this.client);
+    this.credentials = credentials;
     this.retries = retries;
   }
 
